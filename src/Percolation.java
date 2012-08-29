1cae95bebdc9131303cbcd489d7311a41b7cdcf4
Wilma yeung <wilma.y@gmail.com>
2012-08-29 11:21:01 -0700

    Ran checkstyle & findbugs

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,13 +1,13 @@
 public class Percolation {
-    
+
     // An alternative solution is to use 1 union find object only.
     // After the system percolates, create a new union find object
     // and then instantiate it with currently opened sites (without
-    // the virtual bottom index.  As the test do not check the time
+    // the virtual bottom index. As the test do not check the time
     // in isFull(), it will optimized for passing auto grader.
     // During instantiation, there would still be 2 copies of union
     // find objects but VM's garbage collection would remove the
-    // unused one afterwards.  Thus instead of having 2 copies
+    // unused one afterwards. Thus instead of having 2 copies
     // all the times, only have 2 copies at one point during the life
     // of the program.
     //
@@ -16,94 +16,92 @@ public class Percolation {
     // WeigtedQuickUnionUF() (auto grader rule).
 
     private WeightedQuickUnionUF mUf, mIsFullUf;
-    private boolean[][] mSites;
-    private int mN;
-    private boolean mPercolates;
-    
-    public Percolation(int n) {
+    private boolean[][]          mSites;
+    private int                  mN;
+    private boolean              mPercolates;
+
+    public Percolation(final int n) {
         mN = n;
-        
+
         // N-by-N grid with 2 virtual sites
         mUf = new WeightedQuickUnionUF(mN * mN + 2);
         mIsFullUf = new WeightedQuickUnionUF(mN * mN + 1);
-        
+
         mSites = new boolean[mN][mN];
-        
+
         for (int i = 0; i < mN; i++) {
             for (int j = 0; j < mN; j++) {
                 mSites[i][j] = false;
             }
         }
-        
+
         mPercolates = false;
-        
     }
 
-    public void open(int i, int j) {
+    public final void open(final int i, final int j) {
         if (i < 1 || j < 1 || i > mN || j > mN) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        
+
         if ((i + 1) <= mN && isOpen(i + 1, j)) {
             mUf.union(site(i, j), site(i + 1, j));
             mIsFullUf.union(site(i, j), site(i + 1, j));
         }
-        
+
         if ((i - 1) >= 1 && isOpen(i - 1, j)) {
             mUf.union(site(i, j), site(i - 1, j));
             mIsFullUf.union(site(i, j), site(i - 1, j));
         }
-        
+
         if (j > 1 && isOpen(i, j - 1)) {
             mUf.union(site(i, j), site(i, j - 1));
             mIsFullUf.union(site(i, j), site(i, j - 1));
         }
-        
+
         if (j < mN && isOpen(i, j + 1)) {
             mUf.union(site(i, j), site(i, j + 1));
             mIsFullUf.union(site(i, j), site(i, j + 1));
         }
-
 
         // Connect to top virtual site
         if (i == 1) {
             mUf.union(0, site(i, j));
             mIsFullUf.union(0, site(i, j));
         }
-        
+
         // Connect to bottom virtual site
         if (i == mN) {
             mUf.union(site(i, j), mN * mN + 1);
         }
-        
+
         mSites[i - 1][j - 1] = true;
     }
-    
-    public boolean isOpen(int i, int j) {
+
+    public final boolean isOpen(final int i, final int j) {
         if (i < 1 || j < 1 || i > mN || j > mN) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        
+
         return mSites[i - 1][j - 1];
     }
-    
-    public boolean isFull(int i, int j) {
+
+    public final boolean isFull(final int i, final int j) {
         if (i < 1 || j < 1 || i > mN || j > mN) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        
+
         return mIsFullUf.connected(0, site(i, j));
     }
-    
-    public boolean percolates() {
+
+    public final boolean percolates() {
         if (!mPercolates) {
             mPercolates = mUf.connected(0, mN * mN + 1);
         }
-        
+
         return mPercolates;
     }
 
-    private int site(int i, int j) {
+    private int site(final int i, final int j) {
         return (i - 1) * mN + j;
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,71 +1,70 @@
-
 public class PercolationStats {
 
     private double[] mX;
-    private double mMean, mStdDev;
+    private double   mMean, mStdDev;
 
     // perform T independent computational experiments on an N-by-N grid
-    public PercolationStats(int n, int t) {
+    public PercolationStats(final int n, final int t) {
         if (n <= 0 || t <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-        
+
         mX = new double[t];
-        
+
         for (int i = 0; i < t; i++) {
             Percolation p = new Percolation(n);
             int openSites = 0;
-            
+
             while (!p.percolates()) {
                 int row, col;
-                
+
                 do {
                     row = StdRandom.uniform(1, n + 1);
                     col = StdRandom.uniform(1, n + 1);
                 } while (p.isOpen(row, col));
-                
+
                 p.open(row, col);
                 openSites++;
             }
-            
+
             mX[i] = ((double) openSites) / (n * n);
         }
-        
+
         mMean = StdStats.mean(mX);
         mStdDev = StdStats.stddev(mX);
     }
-    
+
     // sample mean of percolation threshold
-    public double mean() {
+    public final double mean() {
         return mMean;
     }
-    
+
     // sample standard deviation of percolation threshold
-    public double stddev() {
+    public final double stddev() {
         return mStdDev;
     }
-       
+
     // test client, described below
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // TODO Auto-generated method stub
         if (args.length < 2) {
             throw new java.lang.IllegalArgumentException();
         }
-        
+
         int n = Integer.parseInt(args[0]);
         int t = Integer.parseInt(args[1]);
-        
+
         PercolationStats ps = new PercolationStats(n, t);
-        
+
         double mean = ps.mean();
         double stddev = ps.stddev();
         System.out.printf("mean                    = %f\n", mean);
         System.out.printf("stddev                  = %f\n", stddev);
-        
+
         final double intervalConstant = 1.96;
         double interval = (stddev * intervalConstant) / Math.sqrt(t);
-        System.out.printf("95%% confidence interval = %f, %f\n",
-                          mean - interval, mean + interval);
+        System.out.printf("95%% confidence interval = %f, %f\n", mean
+                - interval, mean + interval);
     }
 
 }
